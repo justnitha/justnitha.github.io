4c6b2cd384953ae66808e55897a0a8e39a34929f
Your Name <you@example.com>
2023-05-16 20:09:33 +0800

    first commit

---
+++ frontend/js/chalenge2.js
@@ -0,0 +1,12 @@
+//script offcanvas navbar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+   
+}
---
+++ js/script.js
@@ -0,0 +1,24 @@
+//multi text
+var typingEffect = new Typed(".multitext", {
+    strings : ["Coder", "Project", "Designer","Student","Portofolio"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
+var typingEffect = new Typed(".multitext2", {
+    strings : ["Coder", "Project", "Designer","Student","Portofolio"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
+
+
+let text = document.getElementById('text');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    text.style.marginTop = value * 1.5 + 'px';
+
+});
