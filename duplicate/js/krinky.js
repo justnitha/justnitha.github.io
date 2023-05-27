f6fe0eb058e000f29b78fd3acf09942c6e68d478
Your Name <you@example.com>
2023-05-27 13:50:16 +0800

    menambahkan latihan baru

---
+++ duplicate/js/krinky.js
@@ -0,0 +1,171 @@
+//script offcanvas navbar
+/*Script Untuk NAvbar*/
+const buttonToogle =document.querySelector('.buttonToogle')
+const mobileMenu = document.querySelector('.mobileMenu')
+
+buttonToogle.addEventListener('click', function(){
+    mobileMenu.classList.toggle('hidden');
+})
+//multi text
+var typingEffect = new Typed(".text", {
+  strings : ["Designer","Devoloper","Designer","Devoloper","Designer","Devoloper","Designer","Devoloper"],
+  typeSpeed : 50,
+  backSpeed : 80,
+  backDelay : 10,
+});
+//scrool
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 200){
+      $('#main').css({
+        "background":"white","t transition":"0.8s",
+      });
+    }else{
+      $('#main').css({
+        "background":"none"
+      });
+    }
+  });
+});
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+};
+
+// Script untuk Lastest Project
+//getting all required elements
+const gallery  = document.querySelectorAll(".iconn"),
+previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+window.onload = ()=>{
+  for (let i = 0; i < gallery.length; i++) {
+    //meneruskan total panjang img ke variabel totalImg
+    let newIndex = i; //pmeneruskan nilai i ke variabel newIndex
+    let clickedImgIndex; //creating new variable
+      gallery[i].onclick = () =>{
+        clickedImgIndex = i; //passing cliked image index to created variable (clickedImgIndex)
+        function preview(){
+         //passing current img index to currentImg varible with adding +1
+          let imageURL = gallery[newIndex].querySelector("img").src; //getting user clicked img url
+            previewImg.src = imageURL; //passing user clicked img url in previewImg src
+            }
+            preview(); //calling above function
+            const prevBtn = document.querySelector(".prev");
+            const nextBtn = document.querySelector(".next");
+            if(newIndex == 0){ //if index value is equal to 0 then hide prevBtn
+              prevBtn.style.display = "none"; 
+            }
+            if(newIndex >= gallery.length - 1){ //if index value is greater and equal to gallery length by -1 then hide nextBtn
+              nextBtn.style.display = "none"; 
+            }
+            prevBtn.onclick = ()=>{ 
+              newIndex--; //decrement index
+              if(newIndex == 0){
+                preview(); 
+                prevBtn.style.display = "none"; 
+              }else{
+                preview();
+                nextBtn.style.display = "block";
+              } 
+            }
+            nextBtn.onclick = ()=>{ 
+              newIndex++; //increment index
+              if(newIndex >= gallery.length - 1){
+                  preview(); 
+                  nextBtn.style.display = "none";
+              }else{
+                  preview(); 
+                  prevBtn.style.display = "block";
+              }
+            }
+            document.querySelector("body").style.overflow = "hidden";
+            previewBox.classList.add("show"); 
+            shadow.style.display = "block"; 
+            closeIcon.onclick = ()=>{
+                newIndex = clickedImgIndex; //assigning user first clicked img index to newIndex
+                prevBtn.style.display = "block"; 
+                nextBtn.style.display = "block";
+                previewBox.classList.remove("show");
+                shadow.style.display = "none";
+                document.querySelector("body").style.overflowY = "scroll";
+            }
+        }
+        
+    } 
+}
+
+// multiple slide show
+// var count = 0;
+// var inc = 0;
+// margin = 0;
+// var slider = document.getElementsByClassName("slider-width")[0];
+// var itemDisplay = 0;
+// if (screen.width > 990) {
+//     itemDisplay = document.getElementsByClassName("slider-container")[0].getAttribute("item-display-d");
+//     margin = itemDisplay * 5;
+// }
+// if (screen.width > 700 && screen.width < 990) {
+//     itemDisplay = document.getElementsByClassName("slider-container")[0].getAttribute("item-display-t");
+//     margin = itemDisplay * 6.8;
+// }
+// if (screen.width > 280 && screen.width < 700) {
+//     itemDisplay = document.getElementsByClassName("slider-container")[0].getAttribute("item-display-m");
+//     margin = itemDisplay * 20;
+// }
+
+
+// var items = document.getElementsByClassName("item");
+// var itemleft = items.length % itemDisplay;
+// var itemslide = Math.floor(items.length / itemDisplay) - 1;
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.width = (screen.width / itemDisplay) - margin + "px";
+// }
+
+// function satu() {
+//   if (inc !== itemslide + itemleft) {
+//     if (inc == itemslide) {
+//       inc = inc + itemleft;
+//       count = count - (screen.width / itemDisplay) * itemleft;
+//       }
+//       else {
+//         inc++;
+//         count = count - screen.width;
+//       }
+//     }
+//     slider.style.left = count + "px";
+// }
+
+// function prev() {
+//     if (inc !== 0) {
+//         if (inc == itemleft) {
+//             inc = inc - itemleft;
+//             count = count + (screen.width / itemDisplay) * itemleft;
+//         }
+//         else {
+//             inc--;
+//             count = count + screen.width;
+//         }
+//     }
+//     console.log(inc)
+//     slider.style.left = count + "px";
+    
+// }
+
+var splides = new Splide( '.splide', {
+  perPage: 2,
+   rewind : true,
+   
+} );
+
+splides.mount();
