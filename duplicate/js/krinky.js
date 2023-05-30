3e3ec962eab922077097b9adf1835cf88da2fe06
Your Name <you@example.com>
2023-05-30 21:24:18 +0800

    menambahkan animasi

--- duplicate/js/krinky.js
+++ duplicate/js/krinky.js
@@ -111,3 +111,20 @@ var splides = new Splide( '.splide', {
 } );
 
 splides.mount();
+
+// WOW
+wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:       100,
+    callback:     function(box) {
+      console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+  }
+);
+wow.init();
+document.getElementById('moar').onclick = function() {
+  var section = document.createElement('section');
+  section.className = 'section--purple wow fadeInDown';
+  this.parentNode.insertBefore(section, this);
+};
